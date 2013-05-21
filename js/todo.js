22f9b5cf75da4284022d3c6b824c59d8cb48d3de
Erik Michaels-Ober <sferik@gmail.com>
2013-05-21 14:53:36 -0700

    Add views

--- js/todo.js
+++ js/todo.js
@@ -37,4 +37,46 @@ $(document).ready(function(){
 
   var Todos = new TodoList;
 
+  var TodoView = Backbone.View.extend({
+    tagName: 'li',
+    template: _.template('<span>' +
+      '<input type="checkbox" class="checkbox"' +
+      '<%= complete ? \' checked="checked"\' : "" %>>' +
+      '<%= title %></span>'),
+    events: {
+      // $('.checkbox').on(click, toggleView);
+      "click .checkbox": "toggleView"
+    },
+    toggleView: function() {
+      this.model.toggle();
+    },
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+var AppView = Backbone.View.extend({
+  el: $('#todoapp'),
+  events: {
+    "click #button": "addTodo"
+  },
+  initialize: function() {
+    this.input = this.$('#new-todo');
+    this.listenTo(Todos, 'add', this.appendTodo);
+    Todos.fetch();
+  },
+  appendTodo: function(todo) {
+    var view = new TodoView({model: todo});
+    this.$('#todo-list').append(view.render().el);
+  },
+  addTodo: function() {
+    // Todo.create({title => "foo"})
+    Todos.create({title: this.input.val()});
+    this.input.val('');
+  }
+});
+
+var App = new AppView;
+
 });
